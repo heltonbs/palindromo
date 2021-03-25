364d112f5d1db8fc6096823922ab950c3b58bfc2
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2021-03-25 09:49:08 -0300

    Arquivos modificados

--- palindromo.rb
+++ palindromo.rb
@@ -4,7 +4,7 @@ palavra_digitada = gets.chomp
 palavra_auxiliar = palavra_digitada.reverse
 
 if palavra_auxiliar.upcase.eql?(palavra_digitada.upcase)
-    puts "A palavra #{palavra_digitada} tem #{palavra_digitada.length} caracteres e é palíndroma."
+    puts "A palavra #{palavra_digitada} tem #{palavra_digitada.length} caracteres e é palíndromo."
 else
-    puts "A palavra #{palavra_digitada} tem #{palavra_digitada.length} caracteres e não é palíndroma."
+    puts "A palavra #{palavra_digitada} tem #{palavra_digitada.length} caracteres e não é palíndromo."
 end
